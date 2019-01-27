4843d2530b6b74a037beec195841c19debabcff2
altf4arnold <arnold.dech@adct.be>
2019-01-27 20:54:24 +0100

    Adding new file that will change depending on the user preference

---
+++ whatwewant.py
@@ -0,0 +1,6 @@
+"""
+This is the file that contains the stop_number. So you don't need to re-edit
+the entire timer file when you git pull for update
+"""
+
+stop_nb= "" #Simply edit this string with the stop number you are interested in
